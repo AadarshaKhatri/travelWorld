efecc57e8b73df155831ae1ddad3f6878e7a006e
Aadarsha Khatri <adarskhatri1@gmail.com>
2024-12-25 20:30:22 +0545

    Home Page Completed

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,7 +2,22 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section>
+      <div className="bg-purple-950 w-full h-60"> 
+
+
+        <div className="w-full flex flex-col justify-center items-center pt-5">
+          <h2 className="text-white text-center text-4xl font-bold">Tours and Travel Booking Project</h2>
+
+          <p className="text-center mt-5 text-white/50 w-[600px]">This is a travel booking website where you can discover various destinations to visit and make bookings for the places that interest you. This is a practice project aimed at developing a full-stack web application!</p>
+
+        </div>
+       
+      
+        
+        
+      </div>
+    </section>
   )
 }
 
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -5,6 +5,7 @@ import TourDetails from "../pages/TourDetails";
 import SignUp from "../pages/SignUp";
 import LogIn from "../pages/LogIn";
 import SearchResultList from "../pages/SeachResultList";
+import About from "../pages/About";
 
 
 const Router = ()=>{
@@ -57,6 +58,7 @@ const Router = ()=>{
       <Route path="/tours/:id" element={<TourDetails/>} />
       <Route path="/register" element={<SignUp/>} />
       <Route path="/login" element={<LogIn/>} />
+      <Route path="/about" element={<About/>}/>
       <Route path="/tours/search" element={<SearchResultList/>}/>
     </Routes>
   )
